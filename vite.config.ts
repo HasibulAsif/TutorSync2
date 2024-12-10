4e6c91add0b0af1a0a215d21d758e0148ea4b9a4
HasibulAsif <80703827+HasibulAsif@users.noreply.github.com>
2024-12-11 03:51:46 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'; 
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Ensures correct path handling for your root-level domain
   plugins: [
     react(),
     {
